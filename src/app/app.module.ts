593cabb03577cf911911822b7c614d20350ca377
ernestas.buta@gmail.com <ernestas.buta@gmail.com>
2021-06-25 21:42:43 +0300

    base implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { DragAndDropManagerDirective, DragAndDropManagerRootDirective } from 'src/_directives/drag-and-drop-manager.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    DragAndDropManagerDirective,
+    DragAndDropManagerRootDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    DragDropModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
